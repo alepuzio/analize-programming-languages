73eae955ce48c8352c90bef5d2de8ec1d16a5ca2
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-26 15:18:36 +0100

    fixed by kagle versio n5 of the notebook

--- ml-qualitative.R
+++ ml-qualitative.R
@@ -1,40 +1,37 @@
-print ("copy dataset")
-dataset.compare.languages.qualitative <- my.safe.copy.dataframe(dataset.compare.languages)
+#Qualitative analisys
+#copy dataset
 
+dataset.compare.languages.qualitative <- my.safe.copy.dataframe(dataset.compare.languages)
 #delete useless columns
+
 dataset.compare.languages.qualitative$Standardized <- NULL
 dataset.compare.languages.qualitative$Use<-NULL
 dataset.compare.languages.qualitative$Other <- NULL
-
-
+#Load libraries to drawing the decision trees
 
 library("rpart")
 library("rattle")
 library("rpart.plot")
 library("RColorBrewer")
+#Loading required package: bitops
+
+#Create train and test dataset using sample() function
 
 #from an idea of https://www.edureka.co/blog/knn-algorithm-in-r/
 set.seed(123)
 dat.d <- sample(1:nrow(dataset.compare.languages.qualitative),size=nrow(dataset.compare.languages.qualitative)*0.7,replace = FALSE) #random selection of 70% data.
 train <- dataset.compare.languages.qualitative[dat.d,] # 70% training data
 test <- dataset.compare.languages.qualitative[-dat.d,] # remaining 30% test data
-
-print("Relation between Functional paradigma and some use cases")
+#Relation between Functional paradigma and some use cases
 formula.functional.use.cases <- Functional ~ Prototype.Based.Programming+ Data.Processing  + Domain.Specific + Symbolic.Computing
 treemodel <- rpart (formula.functional.use.cases, data = train, method = "class", minsplit=2, minbucket=1)
 prediction <- predict (treemodel, newdata = test, type = 'class')
 table(prediction, test$Functional)
 fancyRpartPlot(treemodel)
 
-
-# Reflective
-print("Relation between Reflective paradigma and some use cases")
-formula.reflective.use.cases <- Reflective ~Event.driven + Business + Domain.Specific + Mobile + GUI + Web+Highly.domain.specific+RAD
+#Relation between Reflective paradigma and some use cases
+formula.reflective.use.cases <- Reflective ~Event.Driven + Business + Domain.Specific + Mobile + GUI + Web+Highly.domain.specific+RAD
 treemodel <- rpart (formula.reflective.use.cases, data = train, method = "class", minsplit=2, minbucket=1)
 prediction <- predict (treemodel, newdata = test, type = 'class')
 table(prediction, test$Reflective)
 fancyRpartPlot(treemodel)
-
-
-
-
--- read.R
+++ read.R
@@ -1,5 +1,6 @@
+
 # load libraries
-library("Amelia")#verify miss values
+library("Amelia")
 library("caret")
 library("corrplot")
 library("cowplot")
@@ -8,12 +9,11 @@ library("funModeling")
 library("ggplot2")
 library("Hmisc")
 library("MASS")
-library("mlbench") #verify miss values
+library("mlbench")
 library("plyr")
 library("rvest")
 library("stringr")
 library("tidyverse") # metapackage of all tidyverse packages
-
 
 # It read the data from wikipedia at the number-th table and 
 # return the data.frame
@@ -21,8 +21,7 @@ library("tidyverse") # metapackage of all tidyverse packages
 # number: number-th of the tabel in the page HTML
 my.raw.wikipedia <- function(url, number) {
   print(paste("my.raw.wikipedia(", url ,",", number ,")"))
-  webpage <- read_html(url)
-  #output "xml_document" "xml_node"
+  webpage <- read_html(url)  #output "xml_document" "xml_node"
   my.raw.wikipedia <- html_table(webpage, fill=TRUE)[[number]]
   my.raw.wikipedia
 }
@@ -54,34 +53,28 @@ my.save.data.on.file <- function(x, name, directory = ".\\") {
     print(paste("Save raw dataframe [", name,"] in disk", sep = ''))
     write.table(x
                 , file = paste(
-                    paste('.\\', directory, sep ="")
-                    , name
-                    ,'.csv'
-                    , sep = '')
+                  paste('.\\', directory, sep ="")
+                  , name
+                  ,'.csv'
+                  , sep = '')
                 , sep = ';'
                 , row.names = TRUE
                 , dec = '.'
     )
   }
 }
+raw.compare.languages <-  read.csv("../input/programminglanguages/raw_compare_languages.csv", sep = ";",fileEncoding="latin1")
+#EDA of the dataset read by web scraping
 
-
-
-#compration 
-url.compare.languages <-'https://en.wikipedia.org/wiki/Comparison_of_programming_languages'
-raw.compare.languages <- my.raw.wikipedia(url.compare.languages , 2)
-
-# TODO error to analizye
-#my.save.data.on.file(x = raw.compare.languages , name = "raw_compare_languages")
-
-#EDA over dataset by web scraping
 dataset.compare.languages <- my.safe.copy.dataframe(raw.compare.languages)
 colnames(dataset.compare.languages ) <- c("Language","Use","Imperative",
                                           "OOP",  "Functional", "Procedural",
-                                          "Generic","Reflective", "Event.driven", 
+                                          "Generic","Reflective", "Event.Driven", 
                                           "Other", "Standardized")
-missmap(dataset.compare.languages) # no missing values
-which(is.na(dataset.compare.languages)) # no NA values
+print("No missing values")
+missmap(dataset.compare.languages) 
+print("No NA values")
+which(is.na(dataset.compare.languages))
 
 dim(dataset.compare.languages) #134X11
 head(dataset.compare.languages ,1)
@@ -89,8 +82,6 @@ colnames(dataset.compare.languages)
 summary(dataset.compare.languages)
 status(dataset.compare.languages)
 
-
-print("analize the values")
 freq(dataset.compare.languages$Imperative)
 freq(dataset.compare.languages$OOP)
 freq(dataset.compare.languages$Functional)
@@ -98,123 +89,68 @@ freq(dataset.compare.languages$Procedural)
 freq(dataset.compare.languages$Generic)
 freq(dataset.compare.languages$Reflective)
 
-# uhm... there are some values with unclear meaning
-# I allow only the values: No, Yes, Parzial
-#manage the empty values
-## Imperative
-dataset.compare.languages[dataset.compare.languages[3] == "",][3] <- "No"
 
-## Object-Oriented
-dataset.compare.languages[dataset.compare.languages[4] == "",][4] <- "No"
-dataset.compare.languages[dataset.compare.languages[4] != "No"
-                          & dataset.compare.languages[4] != "Yes"
-                          ,][4]  <- "Parzial"
+dataset.compare.languages$Imperative = ifelse(dataset.compare.languages$Imperative == 'Yes',10,0)
 
-#TODO sistemarle
-#my.explicit.dataset <- function(number.column) {
-#  print(paste("my.explicit.dataset(", number.column ,")"))
-#  dataset.compare.languages[dataset.compare.languages[number.column] == ""][number.column] <- "No"
-#  dataset.compare.languages[dataset.compare.languages[number.column] != "No" 
-#                            & dataset.compare.languages[number.column] != "Yes"][number.column] <- "Parzial"
-#}
-  
+dataset.compare.languages$OOP = ifelse(dataset.compare.languages$OOP == 'Yes',10,ifelse(dataset.compare.languages$OOP == 'No',0,5))
+#Functional
 
+dataset.compare.languages$Functional = ifelse(dataset.compare.languages$Functional == 'Yes',10,ifelse(dataset.compare.languages$Functional == 'No',0,5))
+#Procedural
 
+dataset.compare.languages$Procedural = ifelse(dataset.compare.languages$Procedural == 'Yes',10,ifelse(dataset.compare.languages$Procedural == 'No',0,5))
+#Generic
 
-## Functional
+dataset.compare.languages$Generic = ifelse(dataset.compare.languages$Generic == 'Yes',10,ifelse(dataset.compare.languages$Generic == 'No',0,5))
+#Reflective
 
-#my.explicit.dataset(5)
-dataset.compare.languages[dataset.compare.languages[5] == "",][5] <- "No"
-dataset.compare.languages[dataset.compare.languages[5] != "No"
-                          & dataset.compare.languages[5] !="Yes" 
-                          ,][5] <- "Parzial"
+dataset.compare.languages$Reflective = ifelse(dataset.compare.languages$Reflective == 'Yes',10,ifelse(dataset.compare.languages$Reflective == 'No',0,5))
+#Event-driven
 
-## "Procedural"
-dataset.compare.languages[dataset.compare.languages[6] == "",][6] <- "No"
-dataset.compare.languages[dataset.compare.languages[6] != "No"
-                          & dataset.compare.languages[6] !="Yes" 
-                          ,][6] <- "Parzial"
-## "Generic"
-dataset.compare.languages[dataset.compare.languages[7] == "",][7] <- "No"
-dataset.compare.languages[dataset.compare.languages[7] != "No"
-                          & dataset.compare.languages[7] !="Yes" 
-                          ,][7] <- "Parzial"
+dataset.compare.languages$Event.Driven = ifelse(dataset.compare.languages$Event.Driven == 'Yes',10,ifelse(dataset.compare.languages$Event.Driven == 'No',0,5))
+#Run freq() after cleaning the data
 
-
-## "Reflective"
-dataset.compare.languages[dataset.compare.languages[8] == "",][8] <- "No"
-dataset.compare.languages[dataset.compare.languages[8] != "No"
-                          & dataset.compare.languages[8] !="Yes" 
-                          ,][8] <- "Parzial"
-
-## "Event-driven"
-
-dataset.compare.languages[dataset.compare.languages[9] == "",][9] <- "No"
-dataset.compare.languages[dataset.compare.languages[9] != "No"
-                          & dataset.compare.languages[9] !="Yes" 
-                          ,][9] <- "Parzial"
-
-## "Other"
-##decompose
-
-
-#"Standardized"
-print("Run freq() after cleaning the data")
 freq(dataset.compare.languages$Imperative)
 freq(dataset.compare.languages$OOP)
 freq(dataset.compare.languages$Functional)
 freq(dataset.compare.languages$Procedural)
 freq(dataset.compare.languages$Generic)
 freq(dataset.compare.languages$Reflective)
-print("It\'s better")
 
-print("After the normalization")
-summary (dataset.compare.languages) #only categorical columns
 
-print("Run table()")
-table(dataset.compare.languages$Imperative)
-table(dataset.compare.languages$OOP)
-table(dataset.compare.languages$Functional)
-table(dataset.compare.languages$Procedural)
-table(dataset.compare.languages$Generic)
-table(dataset.compare.languages$Reflective)
+#It's better. After the normalization
 
-print("Run describe()")
 describe(dataset.compare.languages)
+dataset.compare.languages 
+#uhm.. ugly representations: I normalize the data
 
-print(" uhm.. ugly representations: I normalize the data")
-dataset.compare.languages$Agile <- ifelse(grepl("agile", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Application <- ifelse(grepl("Application", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Business <- ifelse(grepl("business", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Client.Side <- ifelse(grepl("client-side", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Data.Processing <- ifelse(grepl("data processing", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Domain.Specific <- ifelse(grepl("domain-specific", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Education <- ifelse(grepl("education", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Embedded <- ifelse(grepl("embedded", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Game <- ifelse(grepl("game", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Game.Scripting <- ifelse(grepl("game scripting", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$General.Purpose <- ifelse(grepl("general", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$General.Purpose <- ifelse(grepl("general purpose", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$GUI <- ifelse(grepl("GUI automation (macros)", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Highly.domain.specific <- ifelse(grepl("Highly domain-specific", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Integration <- ifelse(grepl("Integration", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Low.Level.Operations <- ifelse(grepl("low-level operations", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Mobile <- ifelse(grepl("mobile", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Parallel.Application <- ifelse(grepl("Parallel application", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Prototype.Based.Programming <- ifelse(grepl("Prototype-based programming", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$RAD <- ifelse(grepl("RAD", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Realtime <- ifelse(grepl("realtime", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Scripting <- ifelse(grepl("scripting", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Server.side <- ifelse(grepl("server-side", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Shell <- ifelse(grepl("Shell", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Symbolic.Computing <- ifelse(grepl("symbolic computing", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$System <- ifelse(grepl("system", dataset.compare.languages$Use), "Yes", "No")
-dataset.compare.languages$Web <- ifelse(grepl("web", dataset.compare.languages$Use), "Yes", "No")
+dataset.compare.languages$Agile <- ifelse(grepl("agile", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Application <- ifelse(grepl("Application", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Business <- ifelse(grepl("business", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Client.Side <- ifelse(grepl("client-side", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Data.Processing <- ifelse(grepl("data processing", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Domain.Specific <- ifelse(grepl("domain-specific", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Education <- ifelse(grepl("education", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Embedded <- ifelse(grepl("embedded", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Game <- ifelse(grepl("game", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Game.Scripting <- ifelse(grepl("game scripting", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$General <- ifelse(grepl("general", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$General.Purpose <- ifelse(grepl("general purpose", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$GUI <- ifelse(grepl("GUI automation (macros)", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Highly.domain.specific <- ifelse(grepl("Highly domain-specific", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Integration <- ifelse(grepl("Integration", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Low.Level.Operations <- ifelse(grepl("low-level operations", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Mobile <- ifelse(grepl("mobile", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Parallel.Application <- ifelse(grepl("Parallel application", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Prototype.Based.Programming <- ifelse(grepl("Prototype-based programming", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$RAD <- ifelse(grepl("RAD", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Realtime <- ifelse(grepl("realtime", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Scripting <- ifelse(grepl("scripting", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Server.side <- ifelse(grepl("server-side", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Shell <- ifelse(grepl("Shell", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Symbolic.Computing <- ifelse(grepl("symbolic computing", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$System <- ifelse(grepl("system", dataset.compare.languages$Use), 10, 0)
+dataset.compare.languages$Web <- ifelse(grepl("web", dataset.compare.languages$Use), 10, 0)
+#Better...
 
-print("better...")
 describe(dataset.compare.languages)
-
-
-
-#tye check evolutvia
-#https://en.wikipedia.org/wiki/Comparison_of_programming_languages_by_type_system
